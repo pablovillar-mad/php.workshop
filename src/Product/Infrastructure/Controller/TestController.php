07f6f7a660597083dcc79888386715886d20468b
pablovillar <pablo.villar@mad.io>
2023-06-07 14:02:46 +0200

    Change php version and created test controller

--- src/Product/Infrastructure/Controller/TestController.php
+++ src/Product/Infrastructure/Controller/TestController.php
@@ -3,8 +3,14 @@
 namespace App\Product\Infrastructure\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class TestController extends AbstractController
 {
+    public function exercise(Request $request): JsonResponse
+    {
+        return new JsonResponse(['message' => 'Hello World!']);
+    }
 
 }
